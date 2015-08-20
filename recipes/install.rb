dc216febb6f5fe3d0e4d7c6035724f0ad9239dc3
Ryan Copley <github@ryancopley.com>
2015-08-20 10:33:24 -0400

    Yay one nation setup works

---
+++ recipes/install.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: git-clone
+# Recipe:: one-nation
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+directory "/usr/share/nginx/html/" do
+  recursive true
+  action :delete
+end
+
+file "/etc/nginx/sites-enabled/000-default" do
+  action :delete
+end
+
+git "/usr/share/nginx/" do
+    repository "git://github.com/GannettDigital/one-nation.git"
+    reference "master"
+    destination "/usr/share/nginx/html"
+    action :sync
+end
+
+service "nginx" do
+  action :restart
+end
